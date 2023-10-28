ac7646abc2f5f13b9bc4c2b823e9c3adfc655f60
unknown <lukasz.bielenin@microsoft.wsei.edu.pl>
2023-10-28 13:46:22 +0200

    add routing module

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={
+        //https://jsonplaceholder.typicode.com/guide/ wzór api!!!
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,7 +24,15 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </div>}>
+        {/* {/* <Route index element={<Home />} />
+        <Route path="blogs" element={<Blogs />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="*" element={<NoPage />} /> */}
+      </Route>
+    </Routes>
+  </BrowserRouter>
+    
   );
 }
 
